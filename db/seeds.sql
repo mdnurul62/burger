08e6753f88501c2c29ba4256f4ab86f13f3a3e39
mdnurul62 <nurul.alam1@hotmail.com>
2017-07-20 12:39:59 -0400

    New files and folder added

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO burgers(burger_name) VALUES("CheeseBurger");
+INSERT INTO burgers(burger_name) VALUES("Double CheeseBurger");
+INSERT INTO burgers(burger_name) VALUES("Hamburger");
+INSERT INTO burgers(burger_name) VALUES("Chicken CheeseBurger");
+INSERT INTO burgers(burger_name, devoured) VALUES("veggie", true);
